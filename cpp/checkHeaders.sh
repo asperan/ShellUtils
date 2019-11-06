b14631441ec2abe927e2725db824ff285d413927
Alex Speranza <alex.speranza@studio.unibo.it>
2019-11-06 12:44:07 +0100

    C++: simplify regular expression; adds TODO; adds comments; adds check to find exit status

--- cpp/checkHeaders.sh
+++ cpp/checkHeaders.sh
@@ -1,25 +1,41 @@
+#Color scheme setup
 GREEN=`tput setaf 2`;
 NO_CLR=`tput sgr0`;
 
-PATTERN="^\.?[a-zA-Z0-9/]*\.h((xx)|(h)|(pp)|())$";
+#Regular expression which returns any file path which has an h, hh, hxx or hpp extension
+PATTERN=".*\.h((xx)|(h)|(pp)|())$";
 
+#The default path is the current directory
 DEFAULT_FIND_PATH="./";
 FIND_PATH=${DEFAULT_FIND_PATH};
 
+#If there is at least one argument, use the first as the path to run "find" in
 if [[ $# -ge 1 ]]; then
   FIND_PATH=$1;
 fi
 
-headerList=`find ${FIND_PATH} -regextype 'egrep' -iregex ${PATTERN}`;
+headerList=`find "${FIND_PATH}" -regextype 'egrep' -iregex ${PATTERN}`;
 
-echo "Headers found:";
-echo "${headerList}";
-echo "";
+#The next part is executed only if find does not have an error.
+if [[ $? -eq 0 ]]; then
+  echo "Headers found:";
+  echo "${headerList}";
+  echo "";
 
-for header in ${headerList}; do
-  echo "Checking ${header} ...";
-  g++ -fsyntax-only ${header};
-  if [[ "$?" -eq "0" ]]; then
-    echo -e "[${GREEN}OK${NO_CLR}] File \"${header}\" has no syntax errors.";
-  fi
-done
+  for header in ${headerList}; do
+    echo "Checking ${header} ...";
+    g++ -fsyntax-only ${header};
+    #Errors are printed anyway if g++ finds one, so this script just confirm when the header is OK.
+    if [[ "$?" -eq "0" ]]; then
+      echo -e "[${GREEN}OK${NO_CLR}] File \"${header}\" has no syntax errors.";
+    fi
+    echo ""
+  done
+fi
+
+
+### What to add
+### Options: 
+### --help which prints an usage message
+### --only-errors which prints only headers which has errors.
+###
